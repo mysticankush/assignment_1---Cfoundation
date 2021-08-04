04d6c2760db4a03fe4ba6a541907b7b9e3e42759
vagrant <vagrant@myvm18>
2021-08-04 21:13:18 +0000

    changes made

--- problem5.c
+++ problem5.c
@@ -1,5 +1,5 @@
 /*
- *  Program to reverse the array
+ *  Program to multiply two matrices
  * Compilation : gcc problem5.c
  * Execution : ./a.out
  *
@@ -7,29 +7,49 @@
  * Assignment_1 -> C Foundation
 */
 #include<stdio.h>
-//function to reverse the array
-void reverse(int a[],int n)
-{
-    //swaping the ith and (n-i+1)th element
-    for(int i=1;i<=n/2;i++)
-    {
-        int temp=a[i];
-        a[i]=a[n-i+1];
-        a[n-i+1]=temp;
-    }
-}
+
 int main() {
-    //taking input the size of the array
-    int n;
-    scanf("%d",&n);
-    //taking input the array
-    int a[n+2];
-    for(int i=1;i<=n;i++)
-    scanf("%d",&a[i]);
-    reverse(a,n);
-    //reversed array
-    for(int i=1;i<=n;i++)
-    {
-        printf("%d ",a[i]);
-    }
+    
+   int size;
+   scanf("%d",&size);
+   int matrix1[size+2][size+2];
+   int matrix2[size+2][size+2];
+   //taking input of matrix1
+   for(int i=1;i<=size;i++)
+   {
+       for(int j=1;j<=size;j++)
+       {
+           scanf("%d",&matrix1[i][j]);
+       }
+   }
+   //taking input of matrix2 and initializing multiplied matrix all elements to 0
+   int multiplied_matrix[size+2][size+2];
+   for(int i=1;i<=size;i++)
+   {
+       for(int j=1;j<=size;j++)
+       {
+           scanf("%d",&matrix2[i][j]);
+           multiplied_matrix[i][j]=0;
+       }
+   }
+   //multiplying the matrix
+   for(int i=1;i<=size;i++)//for row[i]
+   {
+       for(int j=1;j<=size;j++)//for column[j]
+       {
+           for(int temp=1;temp<=size;temp++)//multiple columns of row[i] with rows of column[j] from 1 to size
+           {
+               multiplied_matrix[i][j]+=matrix1[i][temp]*matrix2[temp][j];
+           }
+       }
+   }
+   //printing the multiplied matrix
+   for(int i=1;i<=size;i++)
+   {
+       for(int j=1;j<=size;j++)
+       {
+           printf("%d ",multiplied_matrix[i][j]);
+       }
+       printf("\n");
+   }
 }
